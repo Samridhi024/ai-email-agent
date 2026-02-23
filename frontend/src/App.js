636f448d1ee92637b18fd34f5bafa79f201457bc
Samridhi Sinha <162716314+Samridhi024@users.noreply.github.com>
2026-02-24 02:13:38 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setResult(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/analyze", {
+      const response = await fetch("https://ai-email-agent-n7sv.onrender.com/analyze", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
